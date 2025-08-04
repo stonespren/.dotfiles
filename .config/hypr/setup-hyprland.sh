7693ca048ca0e37c50fff6919a96a75daac5ac83
parkerstone <parker@rx.co>
2025-08-04 09:43:28 -0600

    hyprland setup

---
+++ .config/hypr/setup-hyprland.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+yay -S --needed hyprpaper hypridle hyprlock xdg-desktop-portal-hyprland hyprpolkitagent hyprsysteminfo hyprland-qt-support hyprcursor hyprutils hyprlang hyprwayland-scanner aquamarine hyprgraphics hyprland-qtutils mako waybar playerctl network-manager-applet pamixer
